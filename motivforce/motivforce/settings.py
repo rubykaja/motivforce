48acd71554b8563896dc87394a267c586a5756dc
Tom Gosling <konnichiwaruby@gmail.com>
2014-03-09 01:34:28 +0000

    heroku deployed

--- motivforce/motivforce/settings.py
+++ motivforce/motivforce/settings.py
@@ -214,3 +214,23 @@ WAGTAIL_SITE_NAME = 'motivforce'
 
 # Override the search results template for wagtailsearch
 WAGTAILSEARCH_RESULTS_TEMPLATE = 'motivforce/search_results.html'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
